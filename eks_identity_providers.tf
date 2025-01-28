9cd1c86d6f4445f6eca66b11f952ca1097a25b2d
Pablo Marques <marques.aln@gmail.com>
2025-01-28 20:11:49 -0300

    feat: suporte a versões 1.28 até 1.31

--- eks_identity_providers.tf
+++ eks_identity_providers.tf
@@ -10,7 +10,7 @@ data "tls_certificate" "eks_tls_certchain" {
 # Create OpenID Provider
 #
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
-  for_each = var.eks_config
+  for_each = { for key, value in var.eks_config : key => value if value["control_plane"]["version"] == "1.28" || value["control_plane"]["version"] == "1.29" }
 
   client_id_list  = toset(
     ["sts.${data.aws_partition.session.dns_suffix}"]
@@ -23,7 +23,7 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
 # Create EKS Identity Provider  
 #
 resource "aws_eks_identity_provider_config" "eks_idp" {
-  for_each = var.eks_config
+  for_each = { for key, value in var.eks_config : key => value if contains(["1.28", "1.29"], value["control_plane"]["version"]) }
 
   cluster_name = local.cluster_name[each.key]
 
@@ -31,12 +31,13 @@ resource "aws_eks_identity_provider_config" "eks_idp" {
     client_id                     = format("sts.%s", data.aws_partition.session.dns_suffix)
     groups_claim                  = try(each.value["control_plane"]["idp_config"]["groups_claim"], null)
     groups_prefix                 = try(each.value["control_plane"]["idp_config"]["groups_prefix"], null)
-    identity_provider_config_name = aws_eks_cluster.eks_cp[
-      local.cluster_id[each.key]
-    ]["name"]
-    issuer_url                    = aws_eks_cluster.eks_cp[
-      local.cluster_id[each.key]
-    ].identity[0].oidc[0].issuer
+    identity_provider_config_name = try(each.value["control_plane"]["idp_config"]["config_name"], null)
+
+    issuer_url = try(
+      each.value["control_plane"]["idp_config"]["issuer_url"],
+      aws_eks_cluster.eks_cp[local.cluster_id[each.key]].identity[0].oidc[0].issuer
+    )
+
     required_claims               = try(each.value["control_plane"]["idp_config"]["required_claims"], null)
     username_claim                = try(each.value["control_plane"]["idp_config"]["username_claim"], null)
     username_prefix               = try(each.value["control_plane"]["idp_config"]["username_prefix"], null)
